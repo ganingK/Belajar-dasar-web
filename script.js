bf725dbcd38a029ff9ebe29c2eb3d7707b8b3b17
idham cholid <holidam33@gmail.com>
2022-08-22 22:23:29 +0700

    new design

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const toggleButton = document.getElementsByClassName('popcorn')[0];
 const navbarLinks = document.getElementsByClassName('navigasi')[0];
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active');
+  navbarLinks.classList.toggle('active')
 });
